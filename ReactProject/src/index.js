c7657af5ae2156f8571487081168be56926a8250
Denetor <gfc-com@mail.ru>
2018-06-12 17:50:26 +0300

    add event on click by developer

--- ReactProject/src/index.js
+++ ReactProject/src/index.js
@@ -8,3 +8,6 @@ const developer = new Developer("Denis", "Mylnikov");
 document.getElementById("developer").innerText = `Этот сайт разработал ${developer.fullName()}`;
 document.getElementById("user").innerText = `${user.fullName()}`;
 
+document.getElementById("developer").addEventListener("click", function () {
+   alert(new Date());
+});
