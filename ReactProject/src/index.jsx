4ff94b020eccf868c830275a19c5ae1520b08738
Denetor <gfc-com@mail.ru>
2018-06-14 20:14:37 +0300

    Changed enter point

--- ReactProject/src/index.jsx
+++ ReactProject/src/index.jsx
@@ -1,24 +1,17 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
-import Menu from "./Menu.jsx";
-import Header from "./Header.jsx";
 
-const menuItems = [
-    {
-        link: "http://yandex.ru", title: "Yandex"
-    },
-    {
-        link: "http://www.mail.ru", title: "Mail"
-    }
-];
+import Layout from "./Components/Layout.jsx";
+
+
 
 class App extends Component{
 
     render(){
         return (
             <div>
-                <Menu items={menuItems}/>
-                <Header size="small"/>
+                <Layout />
+                {/*<Menu items={menuItems}/>*/}
             </div>
 
         )
