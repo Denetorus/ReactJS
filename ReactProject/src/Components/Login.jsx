bcafc62e4ea048700d88f5002f7c638df6fde54f
Denetor <gfc-com@mail.ru>
2018-06-15 12:32:23 +0300

    Add Modal window for Sign in

--- ReactProject/src/Components/Login.jsx
+++ ReactProject/src/Components/Login.jsx
@@ -1,12 +1,20 @@
 
 import React, { Component } from "react";
+import Modal from "./Modal.jsx";
 
 export default class Login extends Component{
+
+    modalShow(){
+        let item = document.getElementById("modal");
+        item.classList.add("modal__show");
+    }
 
     render(){
         return (
             <div>
-                <div className="btn btn-success">Sign in</div>
+                <Modal/>
+                <div className="btn btn-success" onClick={this.modalShow} >Sign in</div>
+
             </div>
         )
     }
---
+++ ReactProject/src/Components/Modal.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./css/Modal.css";
+
+export default class Modal extends Component{
+
+    modalClose(){
+        let item = document.getElementById("modal");
+        item.classList.remove("modal__show");
+    }
+
+    render(){
+        return (
+            <div className="modal" id="modal">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h4 className="modal-title">SIGN IN</h4>
+                            <button type="button" className="close" onClick={ this.modalClose }><span
+                                aria-hidden="true">&times;</span></button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="container-">
+                                <div className="row">
+                                    <label htmlFor="Login" className="col-3">Login</label>
+                                    <input type="text" id="Login" className="col-8"></input>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <label htmlFor="Password" className="col-3">Password</label>
+                                <input type="password" id="Password" className="col-8"></input>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" onClick={ this.modalClose }>Close</button>
+                            <button type="button" className="btn btn-primary" onClick={ this.modalClose }>Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+
+
+    }
+}
+
