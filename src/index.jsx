5526d5baed2bdd0c27f6dc9c8f05113ecefe193d
Denetor <gfc-com@mail.ru>
2018-06-19 14:02:52 +0300

    cleaning

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+
+import Layout from "./Components/Layout/Layout.jsx";
+
+
+
+class App extends Component{
+
+    render(){
+        return (
+            <div>
+                <Layout />
+                {/*<Menu items={menuItems}/>*/}
+            </div>
+
+        )
+    }
+
+}
+
+ReactDom.render(<App/>, document.getElementById('app'));
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry:{
-        main: path.resolve(__dirname, 'src/index.js'),
+        main: path.resolve(__dirname, 'src/index.jsx'),
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.[chunkhash].js'
     },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+        alias: {
+            components: path.resolve(__dirname, 'src/Components/'),
+        }
+    },
     module:{
         rules:[
             {
-                test: /\.js$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use:{
                     loader: "babel-loader"
                 }
             },
             {
-                test: /\.css$/,
+                test: /\.s?css$/,
                 use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
-                    use: ["css-loader"]
+                    use: ["css-loader", "postcss-loader", "sass-loader"]
                 })
             }
         ],
