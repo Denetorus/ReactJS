6729dcd938b2b6d11d9cd472d93e5fedb3d11738
Denetor <gfc-com@mail.ru>
2018-06-22 13:07:32 +0300

    Add comments

---
+++ src/Components/Comments/Comments.jsx
@@ -0,0 +1,11 @@
+import React, {PureComponent} from 'react';
+
+export default class Comments extends PureComponent {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
